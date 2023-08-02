16f26996431c6d57bf0398d155a4e70e64b9f23b
dmieter <32790897+dmieter@users.noreply.github.com>
2023-08-02 14:00:13 +0300

    show autoupload times

--- src/listats_upload.py
+++ src/listats_upload.py
@@ -1,10 +1,12 @@
 # %% MAIN
 import listats as ls
 import sys
+from datetime import date
  
 N = int(sys.argv[1])  # number of recent tournaments to load
 if N > 100:
     N = 100
 
-if N > 0:    
+if N > 0:
+    print('Loading {} tournaments at {}'.format(N, date.today()))    
     ls.loadTeamTournamentsFromUrl(N)
