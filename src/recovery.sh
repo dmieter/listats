6b131e3755c2f78746a375dd94519c6a5615cddc
dmieter <32790897+dmieter@users.noreply.github.com>
2023-09-13 22:07:29 +0300

    Update recovery.sh for proper logging

--- src/recovery.sh
+++ src/recovery.sh
@@ -2,12 +2,18 @@
 #echo RECOVERY SCRIPT STARTED
 
 process_cnt=$(ps -ef | grep -v 'recovery\|grep' | grep -c "$1")
-echo $process_cnt
+#echo $process_cnt
 
 if ((process_cnt < 1))
 then
-  cd /home/listats/listats/src	
+  cd /home/listats/listats/src
   nohup $1 &
-  echo RECOVERED $1 at $(date)
-else echo ALL GOOD
-fi	
+  echo RECOVERED $1 at $(date) >> recovery.log
+#else echo ALL GOOD
+fi
+
+
+## CRON EXAMPLES
+#*/5 * * * *     /home/listats/listats/src/recovery.sh 'python3 listats-ui.py torpedo 5003'
+#*/5 * * * *     /home/listats/listats/src/recovery.sh 'python3 listats-ui.py ecosystem 5004'
+
