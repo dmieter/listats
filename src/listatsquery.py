40af75ea4199bec7fb23faeb48b184c492268d8c
dmieter <32790897+dmieter@users.noreply.github.com>
2025-03-28 17:25:17 +0300

    recalculate player score from 1 march 2025

--- src/listatsquery.py
+++ src/listatsquery.py
@@ -262,9 +262,16 @@ def calcInternalRatingForRow(row):
     if(row.place <= 3):    
         leader_score = leader_score + 3 - row.place + 1
 
+    
     #print("For player {} with inner place {}, total place {}, leaders {} and players {} score is: {}".format(row.playerName, row.innerPlace, row.place, row.leadersNum, row.playersNum, leader_score))
-    if 'Bundesliga'.lower() in row.eventName.lower() or 'Lichess Liga '.lower() in row.eventName.lower() or 'Rapid League '.lower() in row.eventName.lower() or 'ROSliga'.lower() in row.eventName.lower() or 'FGMClub Mega'.lower() in row.eventName.lower():
+    
+    recalculation_date_1 = date(2025, 3, 1)
+    if row.date >= recalculation_date_1:
+        if 'Bundesliga'.lower() in row.eventName.lower() or 'Lichess Liga '.lower() in row.eventName.lower() or 'Rapid League '.lower() in row.eventName.lower() or 'ROSliga'.lower() in row.eventName.lower() or 'FGMClub Mega'.lower() in row.eventName.lower():
+            leader_score = leader_score * 2
+    elif 'Bundesliga'.lower() in row.eventName.lower() or 'Lichess Liga '.lower() in row.eventName.lower():
         leader_score = leader_score * 2
+
 
     return leader_score
 
